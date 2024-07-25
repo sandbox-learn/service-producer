3a34b306a89f016149a2775b58e46fe645131deb
aassalax <aassali.dev@gmail.com>
2024-07-25 16:55:22 +0200

    produce POJO

--- src/main/kotlin/com/sandbox/rabbitmq/service_producer/config/MessagingConfig.kt
+++ src/main/kotlin/com/sandbox/rabbitmq/service_producer/config/MessagingConfig.kt
@@ -1,6 +1,12 @@
 package com.sandbox.rabbitmq.service_producer.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.amqp.core.FanoutExchange
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -9,5 +15,20 @@ class MessagingConfig {
     @Bean
     fun fanout(): FanoutExchange {
         return FanoutExchange("sandbox.fanout")
+    }
+
+    @Bean
+    fun jackson2JsonMessageConverter(): Jackson2JsonMessageConverter {
+        val objectMapper = ObjectMapper()
+            .registerModule(KotlinModule.Builder().build())
+            .registerModule(JavaTimeModule())
+        return Jackson2JsonMessageConverter(objectMapper)
+    }
+
+    @Bean
+    fun rabbitTemplate(connectionFactory: ConnectionFactory, converter: Jackson2JsonMessageConverter): RabbitTemplate {
+        val rabbitTemplate = RabbitTemplate(connectionFactory)
+        rabbitTemplate.messageConverter = converter
+        return rabbitTemplate
     }
 }
